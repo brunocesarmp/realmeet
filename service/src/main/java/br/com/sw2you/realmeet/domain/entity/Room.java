b421fd28ddd7f1a33578e95380158e23d1f90044
Bruno Cesar <bcesar.bruno@gmail.com>
2022-03-09 10:50:50 -0300

    Criando tabela e entidade Room

---
+++ service/src/main/java/br/com/sw2you/realmeet/domain/entity/Room.java
@@ -0,0 +1,125 @@
+package br.com.sw2you.realmeet.domain.entity;
+
+import static java.util.Objects.isNull;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.util.Objects;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "room")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "seats", nullable = false)
+    private Integer seats;
+
+    @Column(name = "active", nullable = false)
+    private Boolean active;
+
+    public Room() {
+    }
+
+    private Room(Long id, String name, Integer seats, Boolean active) {
+        this.id = id;
+        this.name = name;
+        this.seats = seats;
+        this.active = active;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        if (isNull(active)) {
+            active = true;
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getSeats() {
+        return seats;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Room room = (Room) o;
+        return Objects.equals(id, room.id)
+                && Objects.equals(name, room.name)
+                && Objects.equals(seats, room.seats)
+                && Objects.equals(active, room.active);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, seats, active);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.JSON_STYLE)
+                .append("id", id)
+                .append("name", name)
+                .append("seats", seats)
+                .append("active", active)
+                .toString();
+    }
+
+    public static final class Builder {
+        private Long id;
+        private String name;
+        private Integer seats;
+        private Boolean active;
+
+        private Builder() {
+        }
+
+        public Builder id(Long id) {
+            this.id = id;
+            return this;
+        }
+
+        public Builder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public Builder seats(Integer seats) {
+            this.seats = seats;
+            return this;
+        }
+
+        public Builder active(Boolean active) {
+            this.active = active;
+            return this;
+        }
+
+        public Room build() {
+            return new Room(id, name, seats, active);
+        }
+
+    }
+
+}
