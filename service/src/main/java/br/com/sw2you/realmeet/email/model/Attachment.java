97923a81ccf02ad263fcfd3a2b761b1a0cd0c9b7
Bruno Cesar <bcesar.bruno@gmail.com>
2022-03-26 10:56:20 -0300

    Implementando envio de Email

---
+++ service/src/main/java/br/com/sw2you/realmeet/email/model/Attachment.java
@@ -0,0 +1,88 @@
+package br.com.sw2you.realmeet.email.model;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+import java.io.InputStream;
+import java.util.Objects;
+
+public class Attachment {
+
+    private final InputStream inputStream;
+    private final String contentType;
+    private final String fileName;
+
+    private Attachment(Builder builder) {
+        this.inputStream = builder.inputStream;
+        this.contentType = builder.contentType;
+        this.fileName = builder.fileName;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public InputStream getInputStream() {
+        return inputStream;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Attachment that = (Attachment) o;
+        return Objects.equals(inputStream, that.inputStream) && Objects.equals(contentType, that.contentType) && Objects.equals(fileName, that.fileName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(inputStream, contentType, fileName);
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.JSON_STYLE)
+                .append("inputStream", inputStream)
+                .append("contentType", contentType)
+                .append("fileName", fileName)
+                .toString();
+    }
+
+    public static final class Builder {
+        private InputStream inputStream;
+        private String contentType;
+        private String fileName;
+
+        private Builder() {
+        }
+
+        public Builder inputStream(InputStream inputStream) {
+            this.inputStream = inputStream;
+            return this;
+        }
+
+        public Builder contentType(String contentType) {
+            this.contentType = contentType;
+            return this;
+        }
+
+        public Builder fileName(String fileName) {
+            this.fileName = fileName;
+            return this;
+        }
+
+        public Attachment build() {
+            return new Attachment(this);
+        }
+
+    }
+
+}
